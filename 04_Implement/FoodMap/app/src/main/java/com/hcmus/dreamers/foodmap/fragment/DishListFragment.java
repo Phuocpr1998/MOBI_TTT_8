150815f74e2fe0884f66f4952ff3d4b546ebd6e1
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-01-11 21:14:57 +0700

    add load more dish button in fragment

--- 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/fragment/DishListFragment.java
+++ 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/fragment/DishListFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -13,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,7 +21,6 @@ import com.google.gson.Gson;
 import com.hcmus.dreamers.foodmap.AddDishActivity;
 import com.hcmus.dreamers.foodmap.EditDishActivity;
 import com.hcmus.dreamers.foodmap.EditRestaurantActivity;
-import com.hcmus.dreamers.foodmap.Model.Catalog;
 import com.hcmus.dreamers.foodmap.Model.Dish;
 import com.hcmus.dreamers.foodmap.Model.Restaurant;
 import com.hcmus.dreamers.foodmap.R;
@@ -35,6 +34,7 @@ public class DishListFragment extends Fragment {
     private static final String TAG = "DishListFragment";
     int selectedRow = -1;
     List<Dish> dishes;
+    List<Dish> visibleDishes;
     Restaurant restaurant;
 
     Context context = null;
@@ -45,6 +45,10 @@ public class DishListFragment extends Fragment {
     DishInfoListAdapter adapter;
     FloatingActionButton fab;
     CoordinatorLayout rootLayout;
+    Button btnLoadMoreDish;
+    int lastVisibleDishIndex = -1; // includesive [0, lastVisibleDishIndex)
+    final int NUMBER_OF_DISH_ITEM_LOAD = 3;     // Số món ăn mỗi lần tải thêm
+    final int DEFAULT_NUMBER_DISH_DISPLAY = 5;      // Số món ăn hiển thị ban đầu
 
     // arbitrary interprocess communication ID (just a nickname!)
     final int IPC_ID = (int) (10001 * Math.random());
@@ -68,6 +72,19 @@ public class DishListFragment extends Fragment {
 
             //Null pointer is already check in host activity
             dishes = restaurant.getDishes();
+
+            if (dishes.size() <= DEFAULT_NUMBER_DISH_DISPLAY)
+            {
+                visibleDishes = new ArrayList<>(dishes) ;
+                lastVisibleDishIndex = dishes.size() - 1; //inclusive
+
+            }
+            else {
+                visibleDishes = new ArrayList<>();
+                visibleDishes.addAll(dishes.subList(0, DEFAULT_NUMBER_DISH_DISPLAY)); // Lấy 5 item dish từ toàn bộ dish
+                lastVisibleDishIndex = DEFAULT_NUMBER_DISH_DISPLAY - 1;              //inclusive
+            }
+
         }
     }
 
@@ -81,16 +98,20 @@ public class DishListFragment extends Fragment {
 
         setup();
         handleClickEvent();
-
+        if (visibleDishes.size() == dishes.size())
+        {
+            btnLoadMoreDish.setVisibility(View.GONE);   // Đã nạp toàn bộ món ăn, ẩn nút bấm đi
+        }
         return rootLayout;
     }
 
     private void setup() {
         dishListView = (ListView) rootLayout.findViewById(R.id.dish_list);
         fab = (FloatingActionButton) rootLayout.findViewById(R.id.fabAddDish);
+        btnLoadMoreDish = rootLayout.findViewById(R.id.btnLoadMoreDish);
 
         adapter = new DishInfoListAdapter(context,
-                R.layout.adapter_dish_info_list, dishes
+                R.layout.adapter_dish_info_list, visibleDishes
         );
 
         dishListView.setAdapter(adapter);
@@ -101,7 +122,7 @@ public class DishListFragment extends Fragment {
         dishListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Dish dish = dishes.get(position);
+                Dish dish = visibleDishes.get(position);
                 selectedRow = position;
                 transferData2NextActivity(dish);
             }
@@ -115,6 +136,27 @@ public class DishListFragment extends Fragment {
                 Intent intent = new Intent(context, AddDishActivity.class);
                 intent.putExtra("rest", (Serializable) restaurant);
                 startActivityForResult(intent, AFA_ID);
+            }
+        });
+
+        btnLoadMoreDish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int fromIndex  = lastVisibleDishIndex + 1;          //inclusive [
+                int toIndex = fromIndex + NUMBER_OF_DISH_ITEM_LOAD; //exclusive    )
+
+                if (toIndex > dishes.size())
+                {
+                    toIndex = dishes.size();
+                }
+                visibleDishes.addAll(dishes.subList(fromIndex, toIndex));
+                lastVisibleDishIndex = toIndex - 1;
+                adapter.notifyDataSetChanged();
+
+                if (visibleDishes.size() == dishes.size())       // Đã tải toàn bộ, ẩn nút loadmore đi
+                {
+                    btnLoadMoreDish.setVisibility(View.GONE);
+                }
             }
         });
     }
@@ -149,10 +191,10 @@ public class DishListFragment extends Fragment {
                     if (!isDelete) {
                         String dishJSON = data.getStringExtra("dishJSON");
                         dish = gson.fromJson(dishJSON, Dish.class);
-                        dishes.set(selectedRow, dish);
+                        visibleDishes.set(selectedRow, dish);
                         Toast.makeText(context, "Cập nhật thành công!", Toast.LENGTH_LONG).show();
                     } else {
-                        dishes.remove(selectedRow);
+                        visibleDishes.remove(selectedRow);
                         Toast.makeText(context, "Xóa thành công!", Toast.LENGTH_LONG).show();
                     }
                     adapter.notifyDataSetChanged();
@@ -165,7 +207,7 @@ public class DishListFragment extends Fragment {
                 String dishJSON = data.getStringExtra("dishJSON");
                 dish = gson.fromJson(dishJSON, Dish.class);
 
-                dishes.add(dish);
+                visibleDishes.add(dish);
                 adapter.notifyDataSetChanged();
             }
         } catch (Exception e) {
